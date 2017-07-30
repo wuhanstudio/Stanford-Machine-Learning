c0e745dfbf6db2bd1d267679a90046e2fbfacea5
Wu Han <silenthill5@live.cn>
2017-07-30 11:04:52 +0800

    Week 6 Finished

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+par = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+minErr = inf;
+
+for i = 1:size(par,2)
+	for j = 1:size(par,2)
+		CTemp = par(i);
+		sigmaTemp = par(j);
+        model = svmTrain(X,y,CTemp,@(x1,x2)gaussianKernel(x1,x2,sigmaTemp)); 
+		predictions = svmPredict(model,Xval);
+		err = mean(double(predictions ~= yval));
+		if(err<minErr)
+				minErr = err;
+				C = CTemp;
+				sigma = sigmaTemp;
+		end
+end
+	
 
 
 
